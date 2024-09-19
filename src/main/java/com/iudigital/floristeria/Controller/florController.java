850e15ac87b38678d2e363203cb8c26090bc07f5
diegolsg <diegos85137@gmail.com>
2024-09-18 19:49:05 -0500

    renombrar carpetas

--- src/main/java/com/iudigital/floristeria/Controller/florController.java
+++ src/main/java/com/iudigital/floristeria/Controller/florController.java
@@ -1,4 +1,4 @@
-package com.example.demo.Controller;
+package com.iudigital.floristeria.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/iudigital/floristeria/DemoApplication.java
+++ src/main/java/com/iudigital/floristeria/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.iudigital.floristeria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
