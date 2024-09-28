45a4a4e83b1c435a9ae150582f23397e45ab28c3
diegolsg <84428337+diegolsg@users.noreply.github.com>
2024-09-27 22:14:10 -0500

    crud GestionPedidos

--- src/main/java/com/iudigital/floristeria/models/finanzas.java
+++ src/main/java/com/iudigital/floristeria/models/finanzas.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,6 +18,11 @@ public class finanzas {
     private Long id;
     @Column(name = "orden_completada")
     private Boolean ordenCompletada;
-
-
+    @Column(name = "detalle_pedido")
+    private String detallePedido;
+    @Column(name = "precios_flores")
+    private BigDecimal precioFlores;
+    @Column(name = "costos_adicionales")
+    private BigDecimal costosAdicionales;
+    private BigDecimal total;
 }
