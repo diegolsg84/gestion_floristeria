835b55976e379e87b1caff609ad75696f484be92
LucasRD26 <lucas.rodriguez@est.iudigital.edu.co>
2024-09-30 20:10:16 +0200

    frontend

--- src/main/java/com/iudigital/floristeria/Controller/InformesController.java
+++ src/main/java/com/iudigital/floristeria/Controller/InformesController.java
@@ -10,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 
 @Controller
+@RequestMapping("/Informe")
 public class InformesController {
     @Autowired
     private FinanzasService finanzasService;
